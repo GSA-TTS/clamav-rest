21ee9250942f9f68da4654110e3c38383ef76482
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-11-21 08:43:26 -0500

    Cherry Pick clamrest.go
    Cherry picks /version

--- clamrest.go
+++ clamrest.go
@@ -23,6 +23,36 @@ func init() {
 	log.SetOutput(ioutil.Discard)
 }
 
+func clamversion(w http.ResponseWriter, r *http.Request) {
+	c := clamd.NewClamd(opts["CLAMD_PORT"])
+
+	version, err := c.Version()
+
+	if err != nil {
+		errJson, eErr := json.Marshal(err)
+		if eErr != nil {
+			fmt.Println(eErr)
+			return
+		}
+		fmt.Fprint(w, string(errJson))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	for version_string := range version {
+		if strings.HasPrefix(version_string.Raw, "ClamAV ") {
+			version_values := strings.Split(strings.Replace(version_string.Raw, "ClamAV ", "", 1), "/")
+			respJson := fmt.Sprintf("{ \"Clamav\": \"%s\" }", version_values[0])
+			if len(version_values) == 3 {
+				respJson = fmt.Sprintf("{ \"Clamav\": \"%s\", \"Signature\": \"%s\" , \"Signature_date\": \"%s\" }", version_values[0], version_values[1], version_values[2])
+			}
+			fmt.Fprint(w, string(respJson))
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
 	c := clamd.NewClamd(opts["CLAMD_PORT"])
 
@@ -84,7 +114,7 @@ func scanPathHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, string(resJson))
 }
 
-//This is where the action happens.
+// This is where the action happens.
 func scanHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	//POST takes the uploaded file(s) and saves it to disk.
@@ -219,6 +249,8 @@ func main() {
 
 	http.HandleFunc("/scan", scanHandler)
 	http.HandleFunc("/scanPath", scanPathHandler)
+	http.HandleFunc("/scanHandlerBody", scanHandlerBody)
+	http.HandleFunc("/version", clamversion)
 	http.HandleFunc("/", home)
 
 	// Prometheus metrics
