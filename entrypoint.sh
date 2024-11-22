1dee404ca08b5880667732814abb66a92538cad0
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-11-22 09:01:41 -0500

    Use $PROXY_SERVER instead of $PROXY_PORT

--- entrypoint.sh
+++ entrypoint.sh
@@ -71,20 +71,32 @@ if [ -z "$(ls -A /clamav/data)" ]; then
   cp /var/lib/clamav/* /clamav/data/
 fi
 
-(
-    freshclam --config-file=/clamav/etc/freshclam.conf --daemon &
-    clamd --config-file=/clamav/etc/clamd.conf &
-    /usr/bin/clamav-rest &
-    # Force reload the virus database through the clamd socket after 120s.
-    # Starting freshclam and clamd async ends up that a newer database version is loaded with
-    # freshclam, but the clamd still keep the old version existing before the update because 
-    # the socket from clamd is not yet ready to inform, what is indicated in the log
-    # during the startup of the container (WARNING: Clamd was NOT notified: Can't connect to clamd through /run/clamav/clamd.sock: No such file or directory).
-    # So only if a newer database version is available clamd will be notified next time, and this can take hours/days.
-    # Remarks: The socket port is configured in the .Dockerfile itself.
-    sleep 120s
-    echo RELOAD | nc 127.0.0.01 3310 &
-) 2>&1 | tee -a /var/log/clamav/clamav.log
+if [ -n "$PROXY_SERVER" ]; then
+    echo "Proxy Detected"
+    (
+        freshclam --config-file=/clamav/etc/freshclam.conf --daemon &
+        clamd --config-file=/clamav/etc/clamd.conf &
+        /usr/bin/clamav-rest &
+    ) 2>&1 | tee -a /var/log/clamav/clamav.log
+else
+    echo "No Proxy Detected"
+    (
+        freshclam --config-file=/clamav/etc/freshclam.conf --daemon &
+        clamd --config-file=/clamav/etc/clamd.conf &
+        /usr/bin/clamav-rest &
+        # Force reload the virus database through the clamd socket after 120s.
+        # Starting freshclam and clamd async ends up that a newer database version is loaded with
+        # freshclam, but the clamd still keep the old version existing before the update because
+        # the socket from clamd is not yet ready to inform, what is indicated in the log
+        # during the startup of the container (WARNING: Clamd was NOT notified: Can't connect to clamd through /run/clamav/clamd.sock: No such file or directory).
+        # So only if a newer database version is available clamd will be notified next time, and this can take hours/days.
+        # Remarks: The socket port is configured in the .Dockerfile itself.
+        sleep 30s
+        echo "RELOAD" | nc 127.0.0.1 3310 &
+    ) 2>&1 | tee -a /var/log/clamav/clamav.log
+fi
+
+
 
 pids=`jobs -p`
 
